e871d3b771d279b459a13ebe213a89a41662556f
fcortesbio <fcortesbio@gmail.com>
2025-12-15 11:41:57 -0500

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+# Networking aliases
+alias hotspot='sudo nmcli device wifi connect "SMA56" password "3118917668'
+alias vicwifi='sudo nmcli device wifi connect "VICTORIA" password "31138259"'
+
+# Hyprland Configs
+alias monitors="nvim ~/.config/hypr/monitors/default.conf"
+alias keyb="nvim ~/.config/hypr/keybindings/default.conf"
+
+# System maintenance
+alias src="source ~/.bashrc"
+alias bashfuncs="nvim ~/bash-custom/"
+
