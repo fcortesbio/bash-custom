e476d0e2348396d651d3b5592ede271942be189f
fcortesbio <fcortesbio@gmail.com>
2025-12-18 02:05:19 -0500

    create alias to update Obsidian notes

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,14 @@
 # Networking aliases
+
+# Quick save for your learning vault
+alias save-notes='cd ~/Desktop/obsidian-notebook && git add -A && git commit -m "Update notes: $(date)" && git push'
 alias hotspot='sudo nmcli device wifi connect "SMA56" password "3118917668"'
 alias vicwifi='sudo nmcli device wifi connect "VICTORIA" password "31138259"'
 
 # Hyprland Configs
 alias monitors="nvim ~/.config/hypr/monitors/default.conf"
-alias keyb="nvim ~/.config/hypr/keybindings/default.conf"
+alias keyb="nvim ~/.config/hypr/conf/keybindings/default.conf"
 
 # System maintenance
 alias src="source ~/.bashrc"
 alias bashfuncs="nvim ~/bash-custom/"
-
