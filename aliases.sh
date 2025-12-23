3a923294b83ef4bf9e163c13f3dd209ba532c186
fcortesbio <fcortesbio@gmail.com>
2025-12-23 13:34:38 -0500

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,15 @@
 # Networking aliases
 
 # Quick save for your learning vault
+alias anyd="anydesk"
 alias save-notes='cd ~/Desktop/obsidian-notebook && git add -A && git commit -m "Update notes: $(date)" && git push'
 alias hotspot='sudo nmcli device wifi connect "SMA56" password "3118917668"'
 alias vicwifi='sudo nmcli device wifi connect "VICTORIA" password "31138259"'
 
 # Hyprland Configs
-alias monitors="nvim ~/.config/hypr/monitors/default.conf"
+alias monitors="nvim ~/.config/hypr/conf/monitors/default.conf"
 alias keyb="nvim ~/.config/hypr/conf/keybindings/default.conf"
+alias custom-bash="nvim ~/bash-custom"
 
 # System maintenance
 alias src="source ~/.bashrc"
