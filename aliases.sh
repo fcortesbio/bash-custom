e4d5b7ff33b0540de5730e5bf4e0b7e295081538
fcortesbio <fcortesbio@gmail.com>
2026-02-20 13:28:29 -0500

    clean unused bash aliases

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,3 @@
-# Networking aliases
-
-# Quick save for your learning vault
-alias anyd="anydesk"
 alias save-notes='cd ~/Desktop/obsidian-notebook && git add -A && git commit -m "Update notes: $(date)" && git push'
-alias hotspot='sudo nmcli device wifi connect "SMA56" password "3118917668"'
-alias vicwifi='sudo nmcli device wifi connect "VICTORIA" password "31138259"'
-
-# Hyprland Configs
-alias monitors="nvim ~/.config/hypr/conf/monitors/default.conf"
-alias keyb="nvim ~/.config/hypr/conf/keybindings/default.conf"
-alias custom-bash="nvim ~/bash-custom"
-
-# System maintenance
+alias cb="nvim ~/bash-custom"
 alias src="source ~/.bashrc"
-alias bashfuncs="nvim ~/bash-custom/"
